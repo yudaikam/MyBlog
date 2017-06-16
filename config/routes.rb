2241ac47f3a2caca67b791a109d6dd096d51895a
Yudai K <yu.football69@gmail.com>
2017-06-16 05:13:48 +0000

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :messages
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root 'messages#index'
+ resources :messages
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
